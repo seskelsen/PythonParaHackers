553b02de3917d7d5a5730370f25e1b97d41bb6e5
Sigmar Eskelsen <seskelsen@gmail.com>
2023-04-06 00:22:53 -0300

    ajuste formatacao

--- Code/classe.py
+++ Code/classe.py
@@ -2,9 +2,10 @@ class Pessoa:
     def __init__(self, nome, sobrenome):
         self.nome = nome
         self.sobrenome = sobrenome
-    
+
     def printarnomecompleto(self):
         return self.nome + " " + self.sobrenome
+
 
 info_pessoa = Pessoa("Sigmar", "Eskelsen")
 
